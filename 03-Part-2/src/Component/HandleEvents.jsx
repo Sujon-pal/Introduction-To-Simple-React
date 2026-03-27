07140a0aeaa6d744af7d1934d0fd6d455a76a6b1
Sujon Pal <dev.sujonpal@gmail.com>
2026-03-27 20:45:28 +0600

    handle events

--- 03-Part-2/src/Component/HandleEvents.jsx
+++ 03-Part-2/src/Component/HandleEvents.jsx
@@ -3,15 +3,20 @@ function HandleEvent() {
     alert("Button clicked!");
   }
 
+  const sum = (num) =>{
+    let result = num + 10;
+    alert(result)
+  }
+
   return (
     <>
       <button className="btn" onClick={HandleClick}> Click 1
       </button> 
-      {/* <button className="btn" onClick={HandleClick}> Click 2 
-      </button>  */}
+      <button className="btn" onClick={() => sum(10)}> Click 2 
+      </button> 
     </>
   );
 }
-export default HandleEvent;
+export default HandleEvent; 
 
 
